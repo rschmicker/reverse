8d12eb35367444fcc0c010c1428aa3098d5d09ff
balon (TJ Balon) <balon@tangoworldwide.net>
2017-01-25 21:27:14 -0500

    Including Zip

--- app/src/main/java/io/reverseengineering/loginapp/LoginActivity.java
+++ app/src/main/java/io/reverseengineering/loginapp/LoginActivity.java
@@ -4,36 +4,26 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.mindrot.jbcrypt.BCrypt;
-
-import java.util.List;
-
 
 /* Login Screen, provided by Android Studio */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
